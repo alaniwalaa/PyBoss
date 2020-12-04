0a94a9e80f0b06141f468198f98e937f64148b11
walaa <w.alani2010@yahoo.com>
2020-12-03 22:47:16 -0800

    main folders created

---
+++ PyBank/main.py
@@ -0,0 +1,23 @@
+# import os module 
+import os
+
+# import module for reading csv files 
+import csv
+
+# import employee_data from csv file 
+csvpath = 'resources/budget_data.csv'
+
+## converting employee rcords to a new format 
+
+
+
+## test data retrieve 
+with open(csvpath) as csvfile:
+
+    csvreader = csv.reader(csvfile, delimiter=',')
+
+    print(csvreader)
+
+  
+    for row in csvreader:
+        print(row)
