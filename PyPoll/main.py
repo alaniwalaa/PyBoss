69ba184a49ff199253eef1f414fb91a0b7f40495
walaa <w.alani2010@yahoo.com>
2020-12-09 23:46:29 -0800

    finding the winner and creating the output file

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -29,9 +29,8 @@ with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter = ',')
     candidate_dict = {} 
     candidate_list = []
-    pre_candidate = None
-    current_candidate = ''
     cand_votes = 0
+    cand_win = []
     
     # loop through rows - start with the second row (skipping the header row)
     next(csvreader)
@@ -43,91 +42,28 @@ with open(csvpath) as csvfile:
          
         candidate_dict[cand_name] = candidate_dict[cand_name] + 1
     
+    '''cand_percent = cand_votes/vote_count
+    candidate_dict[cand_name] = candidate_dict[cand_name] + cand_percent
+    print(cand_percent)''' 
+   
     print(candidate_dict)
 
+    # getting the max votes for the winner
+    cand_win = max(candidate_dict, key = candidate_dict.get)
+    print('Winner: ', cand_win)
     
-    csvfile.close()
-
-
-
-
-
-'''## test data retrieve 
-with open(csvpath) as csvfile:
-
-    csvreader = csv.reader(csvfile, delimiter=',')
-
-    print(csvreader)
-
-  
-    for row in csvreader:
-        print(row)'''
-
-
-'''# import os module 
-
-# 2- calculate the total amount of Profit/Losses
-with open(csvpath) as csvfile:
-    csvreader = csv.reader(csvfile, delimiter = ',')
-     
-    # loop through rows - start with the second row (skipping the header row)
-    next(csvreader)
-    for row in csvreader:
-        net_total = int(net_total) + int(row[1])
-         
-    print("Total: $", net_total)
-    csvfile.close()
-
-# 3- calculating changes in profit/losses and finding average
-with open(csvpath) as csvfile:
-    csvreader = csv.reader(csvfile, delimiter = ',')
-    
-    
-    previous_row_value = None
-    # loope through rows - skipping the header row 
-    next(csvreader)
-    for row in csvreader:
-        if previous_row_value == None:
-            previous_row_value = float(row[1])
-            continue
-        current_row_value = float(row[1])
-        changes = current_row_value - previous_row_value
-        sum_of_changes = sum_of_changes + changes
-
-# 4- greatest increase / decrease 
-        # finding the INCREASE 
-        if gr_increase < changes:
-            gr_increase = changes
-            date_increase = row[0]
-
-
-        # finding the DECREASE
-        if gr_decrease > changes:
-            gr_decrease = changes
-            date_decrease = row[0]
-
-
-        # update previous row
-        previous_row_value = current_row_value
-        ave_change = sum_of_changes/(date_count-1)
-    
-    print("Average Change: $", "{:.2f}".format(ave_change))
-    print("Greatest Increase in Profits: ", date_increase, "$", gr_increase)
-    print("Greatest Decrease in Profits: ", date_decrease, "$", gr_decrease)
-
     csvfile.close()
 
 
 # export the outcomes of the dataset to a csv file 
-with open('analysis/analized_budget_data.csv', 'w') as csvfile:
+with open('analysis/analized_voters_data.csv', 'w') as csvfile:
 
     # Initialize csv.writer
     csvwriter = csv.writer(csvfile, delimiter=',')
 
     csvwriter.writerow([{
-        'Total Monthe': date_count,
-        'Total: $': net_total,
-        'Average Change:': ave_change, 
-        'Greatest Increase in Profits: ': gr_increase, 
-        'Greatest Decrease in Profits: ': gr_decrease
-    }])'''
+        'Total Votes': vote_count,
+        cand_name: cand_votes,
+        'Winner: ': cand_win
+
+    }])
